48d8ef0c55c2da7318da8aeefd5e1027880600cd
mliu <pkulium@gmail.com>
2023-10-03 15:00:07 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/models.py

--- src/models.py
+++ src/models.py
@@ -12,7 +12,9 @@ class CNN_MNIST(nn.Module):
     def __init__(self):
         super(CNN_MNIST, self).__init__()
         self.conv1 = nn.Conv2d(1, 32, kernel_size=(3,3))
+        self.bn1 = nn.BatchNorm2d(32)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=(3,3))
+        self.bn2 = nn.BatchNorm2d(64)
         self.max_pool = nn.MaxPool2d(kernel_size=(2, 2))
         self.drop1 = nn.Dropout2d(p=0.5)
         self.fc1 = nn.Linear(9216, 128)
@@ -20,8 +22,8 @@ class CNN_MNIST(nn.Module):
         self.fc2 = nn.Linear(128, 10)
         
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
         x = self.max_pool(x)
         x = x.view(-1, x.shape[1]*x.shape[2]*x.shape[3])
         x = self.drop1(x)
@@ -34,8 +36,11 @@ class CNN_CIFAR(nn.Module):
     def __init__(self):
         super(CNN_CIFAR, self).__init__()
         self.conv1 = nn.Conv2d(3,   64,  3)
+        self.bn1 = nn.BatchNorm2d(64)
         self.conv2 = nn.Conv2d(64,  128, 3)
+        self.bn2 = nn.BatchNorm2d(128)
         self.conv3 = nn.Conv2d(128, 256, 3)
+        self.bn3 = nn.BatchNorm2d(256)
         self.pool = nn.MaxPool2d(2, 2)
         self.drop1 = nn.Dropout2d(p=0.5)
         self.fc1 = nn.Linear(64 * 4 * 4, 128)
@@ -45,9 +50,9 @@ class CNN_CIFAR(nn.Module):
         self.fc3 = nn.Linear(256, 10)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = self.pool(F.relu(self.conv3(x)))
+        x = self.pool(F.relu(self.bn1(self.conv1(x))))
+        x = self.pool(F.relu(self.bn2(self.conv2(x))))
+        x = self.pool(F.relu(self.bn3(self.conv3(x))))
         x = x.view(-1, 64 * 4 * 4)
         x = self.drop1(x)
         x = F.relu(self.fc1(x))
