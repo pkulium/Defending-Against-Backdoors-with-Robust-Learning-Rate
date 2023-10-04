13b4679c64c5eb50919f49761839d7c262ab8c1a
mliu <pkulium@gmail.com>
2023-10-04 09:11:24 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/agent.py

--- src/agent.py
+++ src/agent.py
@@ -120,7 +120,7 @@ class Agent():
         mask_optimizer = torch.optim.SGD(mask_params, lr=self.local_model.mask_lr, momentum=0.9)
         noise_params = [v for n, v in parameters if "neuron_noise" in n]
         noise_optimizer = torch.optim.SGD(noise_params, lr=self.local_model.anp_eps / self.local_model.anp_steps)
-        for epoch in range(5):
+        for epoch in range(2):
             train_loss, train_acc = mask_train(model=self, criterion=criterion, data_loader=self.train_loader,
                                         mask_opt=mask_optimizer, noise_opt=noise_optimizer)
         return self.local_model
