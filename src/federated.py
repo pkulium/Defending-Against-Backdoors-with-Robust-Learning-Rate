96568592c0f29731a7efaa73b313185583556232
pkulium <54947136+pkulium@users.noreply.github.com>
2023-10-24 15:10:05 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/federated.py

--- src/federated.py
+++ src/federated.py
@@ -120,7 +120,7 @@ if __name__ == '__main__':
     for rnd in range(1, 2):
         rnd_global_params = parameters_to_vector(global_model.parameters()).detach()
         agent_updates_mask = {}
-        select = [i for i in range(1, 2)]
+        select = [i for i in range(1, args.num_agents)]
         for agent_id in select:
             mask_values = agents[agent_id].train_mask(global_model, criterion)
             agent_updates_mask[agent_id] = mask_values
