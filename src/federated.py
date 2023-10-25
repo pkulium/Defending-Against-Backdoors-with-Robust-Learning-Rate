791ad6e790dea4285ae9ea35d771ea598718a473
pkulium <54947136+pkulium@users.noreply.github.com>
2023-10-25 10:53:42 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/federated.py

--- src/federated.py
+++ src/federated.py
@@ -128,7 +128,7 @@ if __name__ == '__main__':
             agent_updates_mask[agent_id] = mask_values
         # aggregate params obtained by agents and update the global params
         mask_values = aggregator.aggregate_mask_min(agent_updates_mask)
-        prune_by_threshold(global_model, mask_values, pruning_max=0.5, pruning_step=0.01)
+        prune_by_threshold(global_model, mask_values, pruning_max=0.95, pruning_step=0.05)
         print(f'mask_values:{mask_values[0]} - {mask_values[100]} - {mask_values[1000]}')
         
         with torch.no_grad():
