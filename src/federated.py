86071ea50e550bcfc286a8ad1944b8e42470f97d
pkulium <54947136+pkulium@users.noreply.github.com>
2023-10-31 08:43:07 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/federated.py

--- src/federated.py
+++ src/federated.py
@@ -119,7 +119,7 @@ if __name__ == '__main__':
         torch.save(global_model.state_dict(), f'/work/LAS/wzhang-lab/mingl/code/backdoor/Defending-Against-Backdoors-with-Robust-Learning-Rate/save/{SAVE_MODEL_NAME}')
         exit()
 
-    rnd = 0
+    rnd = 1
     with torch.no_grad():
         val_loss, (val_acc, val_per_class_acc) = utils.get_loss_n_accuracy(global_model, criterion, val_loader, args)
         writer.add_scalar('Validation/Loss', val_loss, rnd)
